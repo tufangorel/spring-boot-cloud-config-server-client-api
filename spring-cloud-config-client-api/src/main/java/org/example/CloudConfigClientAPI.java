513197419d8a16cf0b7bfa50a2d55ed850b9f2eb
tufangorel <tufang@gmail.com>
2023-11-12 17:46:24 +0300

    Add @Value("${user.role}") to read property from remote github backend.

--- spring-cloud-config-client-api/src/main/java/org/example/CloudConfigClientAPI.java
+++ spring-cloud-config-client-api/src/main/java/org/example/CloudConfigClientAPI.java
@@ -1,8 +1,11 @@
 package org.example;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @SpringBootApplication
@@ -10,6 +13,15 @@ public class CloudConfigClientAPI {
 
     public static void main(String[] args) {
         SpringApplication.run(CloudConfigClientAPI.class, args);
+    }
+
+    @Value("${user.role}")
+    private String role;
+
+
+    @GetMapping( value = "/whoami/{username}" )
+    public String whoami(@PathVariable("username") String username) {
+        return String.format("Hello! You're %s and you'll become a(n) %s...\n", username, role);
     }
 
 }
