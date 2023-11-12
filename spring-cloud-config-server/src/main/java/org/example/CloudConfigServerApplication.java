1a0d559e908f99b03732847ec42c8b55a1174cba
tufangorel <tufang@gmail.com>
2023-11-12 17:26:08 +0300

    Add CloudConfigServerApplication class.

---
+++ spring-cloud-config-server/src/main/java/org/example/CloudConfigServerApplication.java
@@ -0,0 +1,16 @@
+package org.example;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class CloudConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigServerApplication.class, args);
+    }
+
+}
